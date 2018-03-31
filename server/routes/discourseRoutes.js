3ccf6af480d933609d62aca8b69c8571039eaa63
Michael Burak <michaelrburak@gmail.com>
2018-03-31 14:00:39 -0400

    discourse variables made uniform

--- server/routes/discourseRoutes.js
+++ server/routes/discourseRoutes.js
@@ -3,17 +3,17 @@ const TheDiscourse = mongoose.model('discourses')
 
 module.exports = app => {
     app.get('/api/thediscourse', async (req, res) => {
-        const tm = await TheDiscourse.find()
-        res.send(tm)
+        const discourse = await TheDiscourse.find()
+        res.send(discourse)
     })
     app.put('/api/thediscourse', async (req, res) => {
         const { text } = req.body
-        const discord = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: text }, { new: true })
-        res.send(discord)
+        const discourse = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: text }, { new: true })
+        res.send(discourse)
     })
     app.put('/api/thediscourseclear', async (req, res) => {
-        const dis = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: '' }, { new: true })
-        res.send(dis)
+        const discourse = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: '' }, { new: true })
+        res.send(discourse)
     })
 
 }
