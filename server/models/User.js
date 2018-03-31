c5ad4c2c3338edc4dc833e1b61826c41b81afc90
Michael Burak <michaelrburak@gmail.com>
2018-03-31 14:00:20 -0400

    users now have handles

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-    twitterId: String
+    screenName: String
 });
 
 mongoose.model('users', userSchema);
