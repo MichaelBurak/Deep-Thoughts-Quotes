469c903fd24db948e713d8edc3ed77c88b6c1d09
Michael Burak <michaelrburak@gmail.com>
2018-03-30 21:53:05 -0400

    tentative route for users to submit new quotes

---
+++ server/routes/submissionRoutes.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Submission = mongoose.model('submissions')
+
+module.exports = app => {
+    app.post('/api/submissions', async (req, res) => {
+        const { text, author } = req.body
+
+        const submission = new Submission({
+            title,
+            body
+            //_user: req.user.handle?
+        });
+
+        try {
+            await submission.save()
+        }
+        catch (err) {
+            res.status(422).send(err);
+        }
+    })
+}
