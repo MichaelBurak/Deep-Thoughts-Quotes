4e3d63846aab99833d62cd9d4f13c83b55563b6b
Michael Burak <michaelrburak@gmail.com>
2018-03-31 19:32:06 -0400

    adding and forming retrieval of quotes and active user

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,8 +1,17 @@
-import axios from "axios";
-import { FETCH_USER } from "./types";
+import axios from 'axios';
+import { FETCH_USER } from './types';
+import { FETCH_QUOTE } from './types';
 
 export const fetchUser = () => async dispatch => {
-  const res = await axios.get("/api/current_user");
+	const res = await axios.get('/auth/current_user');
+	console.log('the user is' + res.data);
 
-  dispatch({ type: FETCH_USER, payload: res.data });
+	dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchQuote = () => async dispatch => {
+	const res = await axios.get('/api/quotes');
+	console.log('the quote is' + res.data[0]);
+
+	dispatch({ type: FETCH_QUOTE, payload: res.data[0] });
 };
--- server/client/src/actions/types.js
+++ server/client/src/actions/types.js
@@ -1,1 +1,3 @@
-export const FETCH_USER = "fetch_user";
+export const FETCH_USER = 'fetch_user';
+export const LOADING_USER = 'loading_user';
+export const FETCH_QUOTE = 'fetch_quote';
