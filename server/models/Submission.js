86f3e79377420a09fe84526c8958df272619731a
Michael Burak <michaelrburak@gmail.com>
2018-03-31 14:01:24 -0400

    submissions now include the user's handle and res.send self

--- server/models/Submission.js
+++ server/models/Submission.js
@@ -3,7 +3,8 @@ const { Schema } = mongoose
 
 const submissionSchema = new Schema({
     text: { type: String, maxlength: 2000 },
-    author: { type: String, maxlength: 140 }
+    author: { type: String, maxlength: 140 },
+    screenName: String
 })
 
 mongoose.model('submissions', submissionSchema)
--- server/routes/submissionRoutes.js
+++ server/routes/submissionRoutes.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose')
+const passport = require('passport')
 const Submission = mongoose.model('submissions')
 
 module.exports = app => {
     app.post('/api/submissions', async (req, res) => {
         const { text, author } = req.body
-
+        const screenName = req.user.screenName
         const submission = new Submission({
             text,
-            author
-            //_user: req.user.handle?
+            author,
+            screenName: screenName
         });
 
         try {
             await submission.save()
+            res.send(submission)
         }
         catch (err) {
             res.status(422).send(err);
