e5ad07efe17029d5f2c48aa1a486efcfe78f7c7f
Michael Burak <michaelrburak@gmail.com>
2018-03-31 13:03:50 -0400

    express auth, passport with config, bodyparser added to index

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+const expressSession = require('express-session')
+const passport = require('passport')
 const bodyParser = require('body-parser')
+require('./services/passport');
 
+require('./models/User')
 require('./models/Quote')
 require('./models/Submission')
 require('./models/Discourse')
@@ -12,12 +16,26 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
+app.use(bodyParser.json());
+
+app.use(expressSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    secret: [keys.cookieKey],
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.get('/', (req, res) => {
     res.send({ helloWorld: "Hello Employers, I know MERN" })
 })
 
 app.use(bodyParser.json());
 
+require('./routes/authRoutes')(app)
 require('./routes/quoteRoutes')(app)
 require('./routes/submissionRoutes')(app)
 require('./routes/discourseRoutes')(app)
