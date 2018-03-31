8608118e8b3b0b7274d638c3f444999ade02023b
Michael Burak <michaelrburak@gmail.com>
2018-03-31 19:32:52 -0400

    initial state with quote and username and fetching them in auth reducer

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,10 +1,30 @@
-import { FETCH_USER } from '../actions/types';
+import { FETCH_USER, FETCH_QUOTE, LOADING_USER } from '../actions/types';
 
-export default function(state = null, action) {
-  switch (action.type) {
-    case FETCH_USER:
-      return action.payload || false;
-    default:
-      return state;
-  }
+const initialState = {
+	userName: {},
+	quote: {},
+	loading: false,
+};
+
+export default function(state = initialState, action) {
+	switch (action.type) {
+		case LOADING_USER:
+			return {
+				...state,
+				loading: true,
+			};
+		case FETCH_USER:
+			return {
+				...state,
+				userName: action.payload,
+				loading: false,
+			};
+		case FETCH_QUOTE:
+			return {
+				...state,
+				quote: action.payload,
+			};
+		default:
+			return state;
+	}
 }
