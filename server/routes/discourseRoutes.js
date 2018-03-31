00a300ff0fa667bea0a37e01b638b47612acdfd0
Michael Burak <michaelrburak@gmail.com>
2018-03-31 14:09:42 -0400

    basic locking off of relevant put and post routes requiring logins

--- server/routes/discourseRoutes.js
+++ server/routes/discourseRoutes.js
@@ -1,17 +1,18 @@
 const mongoose = require('mongoose')
 const TheDiscourse = mongoose.model('discourses')
+const requireLogin = require('../middlewares/requireLogin')
 
 module.exports = app => {
     app.get('/api/thediscourse', async (req, res) => {
         const discourse = await TheDiscourse.find()
         res.send(discourse)
     })
-    app.put('/api/thediscourse', async (req, res) => {
+    app.put('/api/thediscourse', requireLogin, async (req, res) => {
         const { text } = req.body
         const discourse = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: text }, { new: true })
         res.send(discourse)
     })
-    app.put('/api/thediscourseclear', async (req, res) => {
+    app.put('/api/thediscourseclear', requireLogin, async (req, res) => {
         const discourse = await TheDiscourse.findOneAndUpdate({ text: { $exists: true } }, { text: '' }, { new: true })
         res.send(discourse)
     })
--- server/routes/submissionRoutes.js
+++ server/routes/submissionRoutes.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 const passport = require('passport')
 const Submission = mongoose.model('submissions')
+const requireLogin = require('../middlewares/requireLogin')
 
 module.exports = app => {
-    app.post('/api/submissions', async (req, res) => {
+    app.post('/api/submissions', requireLogin, async (req, res) => {
         const { text, author } = req.body
         const screenName = req.user.screenName
         const submission = new Submission({
