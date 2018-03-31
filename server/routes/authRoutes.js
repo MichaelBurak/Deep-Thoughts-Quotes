446ee06c7b705d958e1683d2561cadc1f53239ee
Michael Burak <michaelrburak@gmail.com>
2018-03-31 13:03:10 -0400

    basic routes for twitter auth

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,23 @@
+const passport = require('passport');
+
+module.exports = app => {
+    app.get('/auth/login',
+        passport.authenticate('twitter'))
+
+    app.get(
+        '/auth/callback',
+        passport.authenticate('twitter', { failureRedirect: '/' }),
+        (req, res) => {
+            res.redirect('/');
+        }
+    );
+
+    app.get('/auth/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/auth/current_user', (req, res) => {
+        res.send(req.user);
+    });
+};
