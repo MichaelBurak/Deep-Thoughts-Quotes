2ca66be0721a1c0f9e0ec00b67eedf7c6e7ed86e
Michael Burak <michaelrburak@gmail.com>
2018-04-01 01:21:15 -0400

    removed console.log debugging from actions

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 import { FETCH_QUOTE } from './types';
+import { FETCH_AUTH_QUOTES } from './types';
 
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/auth/current_user');
-	console.log('the user is' + res.data);
-
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
 export const fetchQuote = () => async dispatch => {
 	const res = await axios.get('/api/quotes');
-	console.log('the quote is' + res.data[0]);
-
 	dispatch({ type: FETCH_QUOTE, payload: res.data[0] });
+};
+
+export const fetchAuthQuotes = au => async dispatch => {
+	const res = await axios.get(`api/quotes/${au}`);
+	dispatch({ type: FETCH_AUTH_QUOTES, payload: res.data });
 };
