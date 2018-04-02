6c45b9bf0b4a5fc261b86158a16c58b4114cdcad
Michael Burak <michaelrburak@gmail.com>
2018-04-02 18:07:31 -0400

    DisplayedQuote now copies quote to user clipboard on a clickbutton

--- server/client/src/components/DisplayedQuote.js
+++ server/client/src/components/DisplayedQuote.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import GeneratorButton from './GeneratorButton';
 import { fetchQuote } from '../actions';
 
-class DisplayedQuote extends Component {
+class DisplayedQuote extends React.Component {
+	handleClick = () => {
+		const q = document.getElementById('mainQuoteText');
+		const range = document.createRange(); // create new range object
+		range.selectNodeContents(q); // set range to encompass desired element text
+		const selection = window.getSelection(); // get Selection object from currently user selected text
+		selection.removeAllRanges(); // unselect any user selected text (if any)
+		selection.addRange(range); // add range to Selection object to select it
+		document.execCommand('copy');
+	};
+
 	render() {
 		return (
-			<div id="mainQuote">
-				"{this.props.auth.quote.text}"
-				<br />Author: {this.props.auth.quote.author}
+			<div id="mainQuote" className="center">
+				<h3 id="mainQuoteText">"{this.props.quote.text}"</h3>
 				<br />
-				<GeneratorButton fetchQuote={this.props.fetchQuote} />
+				<h3> Author: {this.props.quote.author} </h3>
+				<br />
+				<GeneratorButton currentQuote={this.props.quote.text} fetchQuote={this.props.fetchQuote} />
+				<button id="clipboardBtn" className="button" onClick={this.handleClick}>
+					Copy to Clipboard
+				</button>
 			</div>
 		);
 	}
 }
 
 function mapStateToProps({ auth }) {
-	return { auth };
+	return { quote: auth.quote };
 }
 
 const mapDispatchToProps = {
