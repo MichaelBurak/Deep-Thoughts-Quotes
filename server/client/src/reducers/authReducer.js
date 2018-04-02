b4b7b54d3b2b22ecf8de4a6568a6ccc12ccad568
Michael Burak <michaelrburak@gmail.com>
2018-04-02 18:04:41 -0400

    removed user & load from reducer for author's quotes & discourse fetch

--- server/client/src/reducers/authReducer.js
+++ server/client/src/reducers/authReducer.js
@@ -1,29 +1,25 @@
-import { FETCH_USER, FETCH_QUOTE, LOADING_USER } from '../actions/types';
+import { FETCH_USER, FETCH_QUOTE, LOADING_USER, FETCH_AUTH_QUOTES, FETCH_DISCOURSE } from '../actions/types';
 
 const initialState = {
-	userName: {},
+	userName: '',
 	quote: {},
 	loading: false,
+	authQuotes: [],
+	theDiscourse: '',
 };
 
 export default function(state = initialState, action) {
 	switch (action.type) {
 		case LOADING_USER:
-			return {
-				...state,
-				loading: true,
-			};
+			return { ...state, loading: true };
 		case FETCH_USER:
-			return {
-				...state,
-				userName: action.payload,
-				loading: false,
-			};
+			return { ...state, userName: action.payload, loading: false };
 		case FETCH_QUOTE:
-			return {
-				...state,
-				quote: action.payload,
-			};
+			return { ...state, quote: action.payload };
+		case FETCH_AUTH_QUOTES:
+			return { ...state, authQuotes: action.payload };
+		case FETCH_DISCOURSE:
+			return { ...state, theDiscourse: action.payload };
 		default:
 			return state;
 	}
