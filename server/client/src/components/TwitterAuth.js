c71b3964ae5e5207e9e34d402d8c72d401680b89
Michael Burak <michaelrburak@gmail.com>
2018-04-02 17:46:04 -0400

    several components commented out to implement twitter auth later

---
+++ server/client/src/components/TwitterAuth.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import TwitterLogin from 'react-twitter-auth';
+
+class TwitterAuth extends React.Component {
+	constructor() {
+		super();
+
+		this.state = { isAuthenticated: false, user: null, token: '' };
+	}
+
+	onSuccess = response => {
+		const token = response.headers.get('x-auth-token');
+		response.json().then(user => {
+			if (token) {
+				this.setState({ isAuthenticated: true, user: user, token: token });
+			}
+		});
+	};
+
+	onFailed = error => {
+		alert(error);
+	};
+
+	logout = () => {
+		this.setState({ isAuthenticated: false, token: '', user: null });
+	};
+
+	render() {
+		let content = !!this.state.isAuthenticated ? (
+			<div>
+				<p>Authenticated</p>
+				<div>{this.state.user.email}</div>
+				<div>
+					<button onClick={this.logout} className="button">
+						Log out
+					</button>
+				</div>
+			</div>
+		) : (
+			<TwitterLogin
+				loginUrl="http://localhost:5000/api/v1/auth/twitter"
+				onFailure={this.onFailed}
+				onSuccess={this.onSuccess}
+				requestTokenUrl="http://localhost:5000//api/v1/auth/twitter/reverse"
+			/>
+		);
+
+		return <div className="TwitterAuth">{content}</div>;
+	}
+}
+
+export default TwitterAuth;
