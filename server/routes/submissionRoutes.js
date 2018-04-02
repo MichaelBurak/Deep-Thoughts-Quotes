6205b4d47d337983e1056d8260602e6d738ae8cd
Michael Burak <michaelrburak@gmail.com>
2018-04-02 17:46:26 -0400

    removal of referneces to twitter handles in submissions

--- server/routes/submissionRoutes.js
+++ server/routes/submissionRoutes.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
-const passport = require("passport");
-const Submission = mongoose.model("submissions");
-const requireLogin = require("../middlewares/requireLogin");
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Submission = mongoose.model('submissions');
+// const requireLogin = require("../middlewares/requireLogin");(UnCommentlater)
 
 module.exports = app => {
-  app.post("/api/submissions", requireLogin, async (req, res) => {
-    const { text, author } = req.body;
-    const screenName = req.user.screenName;
-    const submission = new Submission({
-      text,
-      author,
-      screenName: screenName
-    });
+	app.post('/api/submissions', async (req, res) => {
+		const { text, author } = req.body;
+		// const screenName = req.user.screenName;
+		const submission = new Submission({
+			text,
+			author,
+			// screenName: screenName,
+		});
 
-    try {
-      await submission.save();
-      res.send(submission);
-    } catch (err) {
-      res.status(422).send(err);
-    }
-  });
+		try {
+			await submission.save();
+			res.send(submission);
+		} catch (err) {
+			res.status(422).send(err);
+		}
+	});
 };
