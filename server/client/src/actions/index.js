894f6fca3846d7f10950e074b5df5ed6129dd958
Michael Burak <michaelrburak@gmail.com>
2018-04-02 18:05:54 -0400

    added fetchupdateclear for discourse and params to fetching quote

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,18 +2,37 @@ import axios from 'axios';
 import { FETCH_USER } from './types';
 import { FETCH_QUOTE } from './types';
 import { FETCH_AUTH_QUOTES } from './types';
+import { FETCH_DISCOURSE } from './types';
 
 export const fetchUser = () => async dispatch => {
 	const res = await axios.get('/auth/current_user');
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
 
-export const fetchQuote = () => async dispatch => {
-	const res = await axios.get('/api/quotes');
+export const fetchQuote = currentQuote => async dispatch => {
+	const res = await axios.get('/api/quotes', {
+		params: currentQuote,
+	});
+
 	dispatch({ type: FETCH_QUOTE, payload: res.data[0] });
 };
 
 export const fetchAuthQuotes = au => async dispatch => {
 	const res = await axios.get(`api/quotes/${au}`);
 	dispatch({ type: FETCH_AUTH_QUOTES, payload: res.data });
+};
+
+export const fetchDiscourse = () => async dispatch => {
+	const res = await axios.get('/api/thediscourse');
+	dispatch({ type: FETCH_DISCOURSE, payload: res.data[0].text });
+};
+
+export const updateDiscourse = discourse => async dispatch => {
+	const res = await axios.put('/api/thediscourse', { text: discourse });
+	dispatch({ type: FETCH_DISCOURSE, payload: res.data.text });
+};
+
+export const clearDiscourse = () => async dispatch => {
+	const res = await axios.put('/api/thediscourseclear');
+	dispatch({ type: FETCH_DISCOURSE, payload: res.data.text });
 };
