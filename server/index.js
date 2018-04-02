721258064bfa4261f367a51a9f8fd483f88893e4
Michael Burak <michaelrburak@gmail.com>
2018-04-02 17:50:46 -0400

    commented out node index references to auth

--- server/index.js
+++ server/index.js
@@ -1,47 +1,58 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const keys = require("./config/keys");
-const expressSession = require("express-session");
-const passport = require("passport");
-const bodyParser = require("body-parser");
-require("./models/User");
+const express = require('express');
+// var cors = require('cors');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+// const expressSession = require('express-session');
+// const passport = require('passport');
+const bodyParser = require('body-parser');
+//require('./models/User');
 
-require("./services/passport");
+// require('./services/passport');
+// require('./services/jwtAuth');
 
-require("./models/Quote");
-require("./models/Submission");
-require("./models/Discourse");
+require('./models/Quote');
+require('./models/Submission');
+require('./models/Discourse');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+// const corsOption = {
+// 	origin: true,
+// 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+// 	credentials: true,
+// 	exposedHeaders: ['x-auth-token'],
+// };
+
+// app.use(cors(corsOption));
+
 app.use(bodyParser.json());
 
-app.use(
-  expressSession({
-    maxAge: 30 * 24 * 60 * 60 * 1000,
-    secret: [keys.cookieKey],
-    resave: true,
-    saveUninitialized: true,
-    cookie: { secure: false }
-  })
-);
+// app.use(
+// 	expressSession({
+// 		maxAge: 30 * 24 * 60 * 60 * 1000,
+// 		secret: [keys.cookieKey],
+// 		resave: true,
+// 		saveUninitialized: true,
+// 		cookie: { secure: false },
+// 	})
+// );
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send({ helloWorld: "Hello Employers, I know MERN" });
+app.get('/', (req, res) => {
+	res.send({ helloWorld: 'Hello Employers, I know MERN' });
 });
 
 app.use(bodyParser.json());
 
-require("./routes/authRoutes")(app);
-require("./routes/quoteRoutes")(app);
-require("./routes/submissionRoutes")(app);
-require("./routes/discourseRoutes")(app);
+//require('./routes/authRoutes');(app)
+require('./routes/quoteRoutes')(app);
+require('./routes/submissionRoutes')(app);
+require('./routes/discourseRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
