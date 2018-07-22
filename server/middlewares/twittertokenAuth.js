8fba8cab9c34993c373cf279cae6cbf28049b602
Michael Burak <michaelrburak@gmail.com>
2018-07-22 12:34:37 -0400

    commenting out more not implemented auth code

--- server/middlewares/twittertokenAuth.js
+++ server/middlewares/twittertokenAuth.js
@@ -1,21 +1,22 @@
-var createToken = function(auth) {
-	return jwt.sign(
-		{
-			id: auth.id,
-		},
-		'my-secret',
-		{
-			expiresIn: 60 * 120,
-		}
-	);
-};
+//not yet implemented
+// var createToken = function(auth) {
+// 	return jwt.sign(
+// 		{
+// 			id: auth.id,
+// 		},
+// 		'my-secret',
+// 		{
+// 			expiresIn: 60 * 120,
+// 		}
+// 	);
+// };
 
-var generateToken = function(req, res, next) {
-	req.token = createToken(req.auth);
-	return next();
-};
+// var generateToken = function(req, res, next) {
+// 	req.token = createToken(req.auth);
+// 	return next();
+// };
 
-var sendToken = function(req, res) {
-	res.setHeader('x-auth-token', req.token);
-	return res.status(200).send(JSON.stringify(req.user));
-};
+// var sendToken = function(req, res) {
+// 	res.setHeader('x-auth-token', req.token);
+// 	return res.status(200).send(JSON.stringify(req.user));
+// };
